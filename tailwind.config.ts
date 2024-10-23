5cd777f6857e9c76150b203c29f4f3387bdd16b1
moricode49 <samhurley4@hotmail.com>
2024-10-23 09:48:19 +0100

    add global colour variables

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,24 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
-  plugins: [],
+	content: [
+		"./pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		extend: {
+			colors: {
+				background: "var(--background)",
+				foreground: "var(--foreground)",
+				cream: "var(--cream)",
+				darkRed: "var(--dark-red)",
+				lightRed: "var(--light-red)",
+				darkBlue: "var(--dark-blue)",
+				lightBlue: "var(--light-blue)",
+			},
+		},
+	},
+	plugins: [],
 };
 export default config;

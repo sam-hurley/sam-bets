e20a8c13c5053f2a9d7691bb544989b440388c09
moricode49 <samhurley4@hotmail.com>
2024-10-23 09:48:55 +0100

    add home page

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,9 @@ export default function RootLayout({
 		<html lang="en">
 			<body>
 				<main>{children}</main>
+				<footer className="h-max mt-auto bg-darkBlue py-2 text-lg text-cream">
+					<div className="text-center">By Sam Hurley</div>
+				</footer>
 			</body>
 		</html>
 	);
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,16 @@
-import Link from "next/link";
+import NavBar from "@/components/navbar.server";
+import SideBar from "@/components/sidebar.server";
+
 export default function Home() {
 	return (
-		<>
-			<h1>Hello, Next.js!</h1>
-			<Link href="/sports">Sports</Link>
-		</>
+		<main className="items-center justify-between text-center bg-darkBlue">
+			<NavBar />
+			<div className="flex flex-row border-2 h-screen">
+				<SideBar />
+				<div className="mt-40 border-2 w-full">
+					<p className="text-cream">Content</p>
+				</div>
+			</div>
+		</main>
 	);
 }

ee2500818f71ed51a869df501fcc86723eee6110
moricode49 <samhurley4@hotmail.com>
2024-11-08 13:10:21 +0000

    add homepage with updated global styling

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,12 +15,17 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
-			<body className="flex">
-				<NavBar />
-				<SideBar />
-				<main>{children}</main>
-				<footer className="h-max mt-auto bg-darkBlue py-2 text-lg text-cream">
-					<div className="text-center">By Sam Hurley</div>
+			<body className="flex flex-col border-blue-500 border-4 min-h-screen h-fit w-full">
+				<div>
+					<NavBar />
+				</div>
+
+				<div className="flex flex-row border-purple-500 border-4 min-h-screen">
+					<SideBar />
+					<main className="border-green-500 border-2 w-3/4">{children}</main>
+				</div>
+				<footer className="hidden inset-y-0 h-max w-full mt-auto bg-darkBlue py-2 text-lg text-cream">
+					<div className="text-center">By Sam Hurley (links)</div>
 				</footer>
 			</body>
 		</html>
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
-export default function Home() {
+export default async function Home() {
 	return (
-		<main className="items-center justify-between text-center bg-darkBlue">
-			<div className="flex flex-row border-2 h-screen w-screen mt-36 ml-72">
-				<p className="text-cream">Content</p>
-			</div>
+		<main className="bg-darkBlue text-center h-full">
+			<p className="text-cream">Content</p>
 		</main>
 	);
 }

34b2e0f6b711259c73d851ec8d96b7667e45db6b
moricode49 <samhurley4@hotmail.com>
2024-11-08 13:14:18 +0000

    add input, submit button, and post to db on submit

--- components/modal.client.tsx
+++ components/modal.client.tsx
@@ -1,9 +1,47 @@
 "use client";
 
+import { postBetTodb } from "@/db/hasura";
+import { Selection } from "@/types/bets";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 
-export default function Modal() {
+export default function Modal(selection: { selection: Selection }) {
+	const [stake, setStake] = useState(0);
+	const [bet, setBet] = useState({
+		odds: "",
+		selection: "",
+		stake: "",
+		username: "",
+		time: "",
+		date: "",
+	});
+
 	const router = useRouter();
+
+	const stakeInput = (e) => {
+		setStake(Number(e.target.value));
+	};
+	const betSubmit = (e) => {
+		setBet({
+			date: selection.selection.date,
+			time: selection.selection.time,
+			username: selection.selection.username,
+			selection: selection.selection.selection,
+			odds: selection.selection.odds,
+			stake: stake.toString(),
+		});
+	};
+
+	if (bet.selection.length !== 0) {
+		console.log(bet, "bet");
+		postBetTodb(bet)
+			.then((data) => {
+				console.log(data);
+			})
+			.catch((error) => {
+				console.error(error);
+			});
+	}
 
 	return (
 		<div className="fixed inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full flex items-center justify-center">
@@ -17,16 +55,28 @@ export default function Modal() {
 					<div className="mt-2 px-7 py-3 flex flex-row justify-between">
 						<p className="text-lg text-lightRed">Stake:</p>
 						<input
+							onChange={stakeInput}
 							type="text"
 							className="w-16 bg-lightBlue text-cream text-center"
 						/>
 					</div>
 					<div className="flex flex-col mt-4 items-center">
+						{bet.selection.length !== 0 && (
+							<div>
+								<p className="text-green-600 mb-5">Submitted!</p>
+								<button
+									className="w-32 mb-5 px-4 py-2 bg-darkBlue text-cream text-base font-medium rounded-md shadow-sm hover:bg-lightBlue focus:outline-none focus:ring-2 focus:ring-gray-300"
+									onClick={() => {
+										router.push("/my-bets");
+									}}
+								>
+									My Bets
+								</button>
+							</div>
+						)}
 						<button
-							onClick={() => {
-								router.push("/my-bets");
-							}}
-							className="w-32 mb-5 px-4 py-2 bg-darkBlue text-cream text-base font-medium rounded-md shadow-sm hover:bg-lightBlue focus:outline-none focus:ring-2 focus:ring-gray-300"
+							onClick={betSubmit}
+							className="w-32 mb-5 px-4 py-2 bg-darkBlue text-cream text-base font-medium rounded-md shadow-sm hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-gray-300"
 						>
 							Submit
 						</button>

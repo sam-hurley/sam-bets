895554274895c849b8926c7482eb8d5489089cac
moricode49 <samhurley4@hotmail.com>
2024-11-08 13:08:59 +0000

    moved fetch function and add popular sports section

--- components/sidebar.server.tsx
+++ components/sidebar.server.tsx
@@ -1,41 +1,143 @@
-import { Sport, UniqueSports } from "@/types";
+import { fetchSports } from "@/api/odds-api";
+import Image from "next/image";
 import Link from "next/link";
 
 export default async function SideBar() {
-	const data = await fetch(
-		`https://api.the-odds-api.com/v4/sports/?apiKey=${process.env.ODDSAPI_API_KEY}`
-	);
-	const sports = await data.json();
-	const uniqueSports: UniqueSports = [{ key: "test" }];
-	sports.forEach((sport: Sport) => {
-		const found = uniqueSports.some((el) => el.group === sport.group);
-		if (!found) {
-			uniqueSports.push(sport);
-		}
-	});
+	const sports = await fetchSports();
+
 	return (
-		<div className="mt-36 border-2 w-72 bg-darkBlue fixed h-screen text-center">
-			<div>
-				<Link href="/" className="text-lightRed font-bold">
-					Home
-				</Link>
-			</div>
-			<div>
-				<Link href="/sports" className="text-lightRed font-bold">
-					Sports
-				</Link>
-			</div>
-			<div>
-				<Link href="/my-bets" className="text-lightRed font-bold">
-					My Bets
-				</Link>
-			</div>
+		<div className="border-2 bg-darkBlue w-1/4">
+			<ul className="text-lightRed font-bold text-lg bg-cream opacity-90">
+				<li>
+					<Link className="hover:text-lightBlue ml-2" href="/">
+						Home
+					</Link>
+				</li>
+				<li>
+					<p className="ml-2">Search</p>
+				</li>
+				<li className="flex">
+					<Image
+						src="/static/betslip-icon.svg"
+						alt="football icon"
+						width={25}
+						height={25}
+						className="mx-2"
+					/>
+					<Link className="hover:text-lightBlue" href="/my-bets">
+						My Bets
+					</Link>
+				</li>
+				<li>
+					<p className="ml-2">Favourites</p>
+				</li>
+				<li>
+					<p className="ml-2">Popular</p>
+					<ul className="text-cream bg-darkBlue font-thin">
+						<li className="flex my-1">
+							<Image
+								src="/static/sports-icons/football.svg"
+								alt="football icon"
+								width={25}
+								height={25}
+								className="mx-2"
+							/>
+							<Link
+								href="/sports/Soccer/soccer_epl"
+								className="hover:text-lightBlue"
+							>
+								English Premier League
+							</Link>
+						</li>
+						<li className="flex my-1">
+							<Image
+								src="/static/sports-icons/football.svg"
+								alt="football icon"
+								width={25}
+								height={25}
+								className="mx-2"
+							/>
+							<Link
+								href="/sports/Soccer/soccer_uefa_champs_league"
+								className="hover:text-lightBlue"
+							>
+								UEFA Champions League
+							</Link>
+						</li>
+						<li className="flex my-1">
+							<Image
+								src="/static/sports-icons/football.svg"
+								alt="football icon"
+								width={25}
+								height={25}
+								className="mx-2"
+							/>
+							<Link
+								href="/sports/Soccer/soccer_efl_champ"
+								className="hover:text-lightBlue"
+							>
+								English Championship
+							</Link>
+						</li>
+						<li className="flex my-1">
+							<Image
+								src="/static/sports-icons/mma.svg"
+								alt="mma icon"
+								width={25}
+								height={25}
+								className="mx-2"
+							/>
+							<Link
+								href="/sports/Mixed%20Martial%20Arts/mma_mixed_martial_arts"
+								className="hover:text-lightBlue"
+							>
+								UFC
+							</Link>
+						</li>
+						<li className="flex my-1">
+							<Image
+								src="/static/sports-icons/football.svg"
+								alt="american football icon"
+								width={25}
+								height={25}
+								className="mx-2"
+							/>
+							<Link
+								href="/sports/American%20Football/americanfootball_nfl"
+								className="hover:text-lightBlue"
+							>
+								NFL
+							</Link>
+						</li>
+						<li className="flex my-1">
+							<Image
+								src="/static/sports-icons/mma.svg"
+								alt="boxing icon"
+								width={25}
+								height={25}
+								className="mx-2"
+							/>
+							<Link
+								href="/sports/Boxing/boxing_boxing"
+								className="hover:text-lightBlue"
+							>
+								Boxing
+							</Link>
+						</li>
+					</ul>
+				</li>
+				<li>
+					<Link className="ml-2 hover:text-lightBlue" href="/sports">
+						Sports
+					</Link>
+				</li>
+			</ul>
 			<ul>
-				{uniqueSports.map((sport) => (
+				{sports.map((sport) => (
 					<li key={sport.key}>
 						<Link
 							href={`/sports/${sport.group}`}
-							className={"text-cream"}
+							className={"text-cream ml-2 hover:text-lightBlue"}
 							key={sport.key}
 						>
 							{sport.group}

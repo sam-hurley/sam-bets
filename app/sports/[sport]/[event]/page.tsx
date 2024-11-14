95a8f4ab664814f98593fbea969cd2cd744361eb
moricode49 <samhurley4@hotmail.com>
2024-11-14 10:41:32 +0000

    add draws to events

--- app/sports/[sport]/[event]/page.tsx
+++ app/sports/[sport]/[event]/page.tsx
@@ -21,55 +21,73 @@ export default async function EventPage(props: {
 	// const events = await fetchOdds(key);
 	const events = fakeData;
 	const formattedDates = formatDates(events);
+	const draw = events[0].bookmakers[0].markets[0].outcomes[2];
 
 	return (
 		<>
-			<div className="text-center bg-darkBlue w-full min-h-screen h-fit">
-				<ul className="flex flex-col items-center">
+			<div className="bg-darkBlue w-full min-h-screen h-fit">
+				<ul className="flex flex-col">
 					{formattedDates.map((date: string) => (
-						<div
-							key={date}
-							className="text-lightRed font-bold p-1 mt-2 mb-0 w-2/3"
-						>
-							{date}
-
-							{events.map((event: Event) => {
-								if (
-									format(new Date(event.commence_time), "dd/MM/yyy") === date
-								) {
-									return (
-										<div
-											key={event.id}
-											className="flex flex-row justify-between items-center"
-										>
-											<OddsButton
-												odds={{
-													selection: event.home_team,
-													odds: event.bookmakers[0].markets[0].outcomes[0]
-														.price,
-													date: date,
-													time: event.commence_time.slice(11, 16),
-													username: "sam333",
-												}}
-											/>
-											<li className="text-cream ">
-												<p>{event.commence_time.slice(11, 16)}</p>
-												{event.home_team} v {event.away_team}
-											</li>
-											<OddsButton
-												odds={{
-													selection: event.away_team,
-													odds: event.bookmakers[0].markets[0].outcomes[1]
-														.price,
-													date: date,
-													time: event.commence_time.slice(11, 16),
-													username: "sam333",
-												}}
-											/>
-										</div>
-									);
-								}
-							})}
+						<div key={date} className="text-lightRed font-bold p-1 mt-2 mb-0">
+							<div className="flex bg-cream opacity-90 p-2">
+								<p className="justify-items-start">{date}</p>
+								<p className="justify-items-end">HOME DRAW AWAY</p>
+							</div>
+							<div>
+								{events.map((event: Event) => {
+									if (
+										format(new Date(event.commence_time), "dd/MM/yyy") === date
+									) {
+										return (
+											<div
+												key={event.id}
+												className="flex flex-row justify-between items-center"
+											>
+												<li className="text-cream ">
+													<p className="text-lightRed">
+														{event.commence_time.slice(11, 16)}
+													</p>
+													{event.home_team} v {event.away_team}
+												</li>
+												<div className="flex ">
+													<OddsButton
+														odds={{
+															selection: event.home_team,
+															odds: event.bookmakers[0].markets[0].outcomes[0]
+																.price,
+															date: date,
+															time: event.commence_time.slice(11, 16),
+															username: "sam333",
+														}}
+													/>
+													{draw && (
+														<OddsButton
+															odds={{
+																selection: event.away_team,
+																odds: event.bookmakers[0].markets[0].outcomes[2]
+																	.price,
+																date: date,
+																time: event.commence_time.slice(11, 16),
+																username: "sam333",
+															}}
+														/>
+													)}
+													<OddsButton
+														odds={{
+															selection: event.away_team,
+															odds: event.bookmakers[0].markets[0].outcomes[1]
+																.price,
+															date: date,
+															time: event.commence_time.slice(11, 16),
+															username: "sam333",
+														}}
+													/>
+												</div>
+											</div>
+										);
+									}
+								})}
+							</div>
 						</div>
 					))}
 				</ul>
